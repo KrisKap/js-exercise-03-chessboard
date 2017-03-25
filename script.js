467ba36ae526747abcce974a17e0ba6eeace4b6e
Krzysztof Kapcia <krzysztofkapcia@gmail.com>
2017-03-26 00:42:08 +0100

    wyrównano komentarze

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-var row_1 = "";				// definiujemy zmienna tekstową która będzie naszym pierwszym wierszem szachownicy
-var row_2 = "";				// definiujemy zmienna tekstową która będzie naszym drugim wierszem szachownicy
-var chessboard = "";		// definiujemy zmienną tekstową której wartośc będzie wyglądać jak szachownica
+var row_1 = "";							// definiujemy zmienna tekstową która będzie naszym pierwszym wierszem szachownicy
+var row_2 = "";							// definiujemy zmienna tekstową która będzie naszym drugim wierszem szachownicy
+var chessboard = "";				// definiujemy zmienną tekstową której wartośc będzie wyglądać jak szachownica
 
 for (i = 0; i < 30; i++) {	// budujeny wiersze szachownicy ze znaków "#" oraz " " w oparciu o parzystość licznika pętli
 
 	if ((i % 2)) {
-		row_1 += "#";		// znaki w wierszu pierwszym występujące na 1, 3, 5... miejscu to "#"
-		row_2 += " ";		// znaki w wierszu drugim występujące na 1, 3, 5... miejscu to " "
+		row_1 += "#";						// znaki w wierszu pierwszym występujące na 1, 3, 5... miejscu to "#"
+		row_2 += " ";						// znaki w wierszu drugim występujące na 1, 3, 5... miejscu to " "
 	}
 	else {
-		row_1 += " ";		// znaki w wierszu pierwszym występujące na 0, 2, 2... miejscu to " "
-		row_2 += "#";		// znaki w wierszu drugim występujące na 0, 2, 4... miejscu to "#".
+		row_1 += " ";						// znaki w wierszu pierwszym występujące na 0, 2, 2... miejscu to " "
+		row_2 += "#";						// znaki w wierszu drugim występujące na 0, 2, 4... miejscu to "#".
 	}
 
 }
 
 for (i = 0; i < 30; i++) {			// tworzymy szachownicę przez naprzemienną konkatenację row_1 oraz row_2 
-	if (!(i % 2)) {					// wraz ze znakiem nowego wiersza.
+	if (!(i % 2)) {								// wraz ze znakiem nowego wiersza.
 		chessboard += row_1 + "\n"; 
 	}
 
