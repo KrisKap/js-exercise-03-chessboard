f6340589f02808e9e3692bd4b11ad9b205e4409b
Krzysztof Kapcia <krzysztofkapcia@gmail.com>
2017-03-26 01:03:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,23 @@ var chessboard = "";				// definiujemy zmienną tekstową której wartośc będz
 
 for (i = 0; i < 30; i++) {	// budujeny wiersze szachownicy ze znaków "#" oraz " " w oparciu o parzystość licznika pętli
 
-	if ((i % 2)) {
-		row_1 += "#";						// znaki w wierszu pierwszym występujące na 1, 3, 5... miejscu to "#"
-		row_2 += " ";						// znaki w wierszu drugim występujące na 1, 3, 5... miejscu to " "
-	}
-	else {
-		row_1 += " ";						// znaki w wierszu pierwszym występujące na 0, 2, 2... miejscu to " "
-		row_2 += "#";						// znaki w wierszu drugim występujące na 0, 2, 4... miejscu to "#".
-	}
-
-}
+  if ((i % 2)) {
+    row_1 += "#";						// znaki w wierszu pierwszym występujące na 1, 3, 5... miejscu to "#"
+    row_2 += " ";						// znaki w wierszu drugim występujące na 1, 3, 5... miejscu to " "
+  }
+  else {
+    row_1 += " ";						// znaki w wierszu pierwszym występujące na 0, 2, 2... miejscu to " "
+    row_2 += "#";						// znaki w wierszu drugim występujące na 0, 2, 4... miejscu to "#".}
+  }
 
 for (i = 0; i < 30; i++) {			// tworzymy szachownicę przez naprzemienną konkatenację row_1 oraz row_2 
-	if (!(i % 2)) {								// wraz ze znakiem nowego wiersza.
-		chessboard += row_1 + "\n"; 
-	}
+  if (!(i % 2)) {								// wraz ze znakiem nowego wiersza.
+    chessboard += row_1 + "\n"; 
+  }
 
-	else {
-		chessboard += row_2 + "\n";
-	}
+  else {
+    chessboard += row_2 + "\n";
+  }
 
 }
 
